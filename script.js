d0498e198cfbd6214bae2a3b00d691a6cd1f21a4
pranit9604 <147598314+pranit9604@users.noreply.github.com>
2024-02-19 15:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+let inputs = document.querySelectorAll("input")
+let first_name = inputs [0].value
+let last_name =inputs[1].value
+let fullName= first_name + " "+ last_name
+	alert(fullName)
 }
+
